4c8b67a5c6111f72ed840dae1162e74a1cefee93
Jing Yu <jing@MacBook-Air.local>
2017-01-08 15:23:51 -0500

    some validations

--- app/models/cream.rb
+++ app/models/cream.rb
@@ -4,7 +4,7 @@ class Cream < ApplicationRecord
   has_many :cream_ingredients
   has_many :ingredients, through: :cream_ingredients
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :price, presence: true
   validates :size, presence: true
 
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@ class Ingredient < ApplicationRecord
   has_many :cream_ingredients
   has_many :creams, through: :cream_ingredients
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
