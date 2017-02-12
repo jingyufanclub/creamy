f55c709436d0fa0ff4bab06f2a961f7cceea343d
deaundrapeek <jingyufanclub@gmail.com>
2017-02-12 18:26:03 -0500

    use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170111225130) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cream_ingredients", force: :cascade do |t|
     t.integer  "cream_id"
     t.integer  "ingredient_id"
