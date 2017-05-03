6d1cecc5557025273451c070f01762ce343d461f
deaundrapeek <jingyufanclub@gmail.com>
2017-05-02 23:00:42 -0400

    start write sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if !User.find_by(name: params[:user][:name].downcase).try(:authenticate, params[:user][:password])
-      flash.now.alert = "invalid login"
-      redirect_to "/login"
-    else
       @user = User.find_by(name: params[:user][:name].downcase)
       session[:user_id] = @user.id
       redirect_to user_path(@user)
