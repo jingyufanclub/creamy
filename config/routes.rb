39986abf587be3c340ef5057f04c499c950a47e3
deaundrapeek <jingyufanclub@gmail.com>
2017-05-04 17:39:45 -0400

    route alias for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to:'static#index'
-  resources :ingredients
   resources :creams
   resources :formats, only: [:show, :index]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :ingredients
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
