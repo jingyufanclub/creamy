273637e1f1b2b3770cc76934e03541e2a89ddf70
deaundrapeek <jingyufanclub@gmail.com>
2017-05-05 14:39:12 -0400

    deploy to heroku

--- app/models/cream.rb
+++ app/models/cream.rb
@@ -4,23 +4,23 @@ class Cream < ApplicationRecord
   belongs_to :format
   accepts_nested_attributes_for :ingredients
 
-  # validates :name, presence: true, uniqueness: { case_sensitive: false }
-  # validates :price, presence: true, numericality: true
-  # validates :size, presence: true, numericality: true
-  #
-  # def cost_per_unit
-  #   '%.2f' % (price.to_f / size.to_f)
-  # end
-  #
-  # def fave
-  #   if favorite
-  #     "&hearts;"
-  #   end
-  # end
-  #
-  # def rotation
-  #   if current_rotation
-  #     "&#9789;"
-  #   end
-  # end
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :price, presence: true, numericality: true
+  validates :size, presence: true, numericality: true
+
+  def cost_per_unit
+    '%.2f' % (price.to_f / size.to_f)
+  end
+
+  def fave
+    if favorite
+      "&hearts;"
+    end
+  end
+
+  def rotation
+    if current_rotation
+      "&#9789;"
+    end
+  end
 end
