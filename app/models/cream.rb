862ecff643d761530940347e71a6d2e9e98550ce
Jing <jingyufanclub@users.noreply.github.com>
2018-12-26 01:21:51 -0500

    Update cream.rb

--- app/models/cream.rb
+++ app/models/cream.rb
@@ -1,5 +1,5 @@
 class Cream < ApplicationRecord
-  has_many :cream_ingredients, -> { order :kind }
+  has_many :cream_ingredients, -> { order kind: :asc }
   has_many :ingredients, through: :cream_ingredients
   belongs_to :format
   accepts_nested_attributes_for :ingredients
